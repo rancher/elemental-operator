ea7884e8df56ca64b8518bef7baae3303b845dd7
Itxaka <igarcia@suse.com>
2022-08-01 17:08:23 +0200

    Produce 2 binaries instead of one (#99)
    * Produce 2 binaries instead of one
    
    This generates a different binary for the register command as to not
    bundle it with the full operator.
    
    On the makefile separated builds have been done for operator and
    registry.
    
    On the docker image a different container is created for the register
    and pushed to a different repo as well.
    
    For releases gorelease generates 2 different binaries.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -19,7 +19,6 @@ package main
 import (
 	displayCmd "github.com/rancher/elemental-operator/cmd/operator/display"
 	operatorCmd "github.com/rancher/elemental-operator/cmd/operator/operator"
-	registerCmd "github.com/rancher/elemental-operator/cmd/operator/register"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -32,7 +31,6 @@ func main() {
 
 	cmd.AddCommand(
 		operatorCmd.NewOperatorCommand(),
-		registerCmd.NewRegisterCommand(),
 		displayCmd.NewDisplayCommand())
 
 	if err := cmd.Execute(); err != nil {
