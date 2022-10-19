1364648b432f2df59437999c45cddef5ce989d39
Itxaka <igarcia@suse.com>
2022-10-19 16:55:21 +0200

    tests: Add k8s 1.24 and default to rancher 2.6.9
    Also drop old 2.6.6 test.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

--- scripts/setup-kind-cluster.sh
+++ scripts/setup-kind-cluster.sh
@@ -26,4 +26,10 @@ set -e
 kubectl cluster-info --context kind-$CLUSTER_NAME
 echo "Sleep to give times to node to populate with all info"
 kubectl wait --for=condition=Ready node/operator-e2e-control-plane
+# Label the nodes with node-role.kubernetes.io/master as it appears that
+# label is no longer added on >=1.24.X clusters while it was set on <=1.23.X
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/kubeadm/2067-rename-master-label-taint
+# https://kubernetes.io/blog/2022/04/07/upcoming-changes-in-kubernetes-1-24/#api-removals-deprecations-and-other-changes-for-kubernetes-1-24
+# system-upgrade-controller 0.9.1 still uses it to schedule pods
+kubectl label nodes --all node-role.kubernetes.io/master=
 kubectl get nodes -o wide
