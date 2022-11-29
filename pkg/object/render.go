7214aa17224aa8555cb12fa0e8e9975abdf3771f
Alexandr Demicev <alexandr.demicev@suse.com>
2022-11-29 10:22:39 +0100

    Remove unused code

--- pkg/object/render.go
+++ pkg/object/render.go
@@ -22,15 +22,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// Renders a map[string]interface{} into a real object
-func Render(src map[string]interface{}, v interface{}) error {
-	d, err := json.Marshal(src)
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(d, v)
-}
-
 // Renders a map[string]runtime.RawExtension{} into a real object
 func RenderRawExtension(src map[string]runtime.RawExtension, v interface{}) error {
 	d, err := json.Marshal(src)
