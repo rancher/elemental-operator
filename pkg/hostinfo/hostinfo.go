446ecfe64447ecda8c796505094866f0e037d151
Itxaka <igarcia@suse.com>
2022-12-13 15:58:53 +0100

    Store hardare data as labels for nodes during registration
    Use the ghw lib to obtain hardware data about the node and use it to
    have a standard set of hardware labels on the registered inventory
    during registration. Otherwise its not possible to obtain this data
    afterwards unless you ssh into the machine and its even more difficult
    to use selectors to include hardware stuff.
    
    Signed-off-by: Itxaka <igarcia@suse.com>

---
+++ pkg/hostinfo/hostinfo.go
@@ -0,0 +1,111 @@
+/*
+Copyright © 2022 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package hostinfo
+
+import (
+	"github.com/jaypipes/ghw"
+	"github.com/jaypipes/ghw/pkg/baseboard"
+	"github.com/jaypipes/ghw/pkg/bios"
+	"github.com/jaypipes/ghw/pkg/block"
+	"github.com/jaypipes/ghw/pkg/chassis"
+	"github.com/jaypipes/ghw/pkg/context"
+	"github.com/jaypipes/ghw/pkg/cpu"
+	"github.com/jaypipes/ghw/pkg/gpu"
+	"github.com/jaypipes/ghw/pkg/memory"
+	"github.com/jaypipes/ghw/pkg/net"
+	"github.com/jaypipes/ghw/pkg/product"
+	"github.com/jaypipes/ghw/pkg/topology"
+)
+
+// HostInfo represents all the host info minus the PCI devices
+// We cannot use ghw.HostInfo directly as that tries to gather the local pci-ids database, which we don't have
+// And fallbacks to download it from the network. Unfortunately that returns an error instead of a warning and
+// prevents us from gathering the rest of the data. Thus, the necessity of having our own struct :/
+// We could drop this if we include the hwdata package in the base OS
+type HostInfo struct {
+	ctx       *context.Context
+	Memory    *memory.Info    `json:"memory"`
+	Block     *block.Info     `json:"block"`
+	CPU       *cpu.Info       `json:"cpu"`
+	Topology  *topology.Info  `json:"topology"`
+	Network   *net.Info       `json:"network"`
+	GPU       *gpu.Info       `json:"gpu"`
+	Chassis   *chassis.Info   `json:"chassis"`
+	BIOS      *bios.Info      `json:"bios"`
+	Baseboard *baseboard.Info `json:"baseboard"`
+	Product   *product.Info   `json:"product"`
+}
+
+// Host returns a pointer to a HostInfo struct that contains fields with
+// information about the host system's CPU, memory, network devices, etc
+func Host(opts ...*ghw.WithOption) (*HostInfo, error) {
+	ctx := context.New(opts...)
+
+	memInfo, err := memory.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	blockInfo, err := block.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	cpuInfo, err := cpu.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	topologyInfo, err := topology.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	netInfo, err := net.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	gpuInfo, err := gpu.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	chassisInfo, err := chassis.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	biosInfo, err := bios.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	baseboardInfo, err := baseboard.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	productInfo, err := product.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &HostInfo{
+		ctx:       ctx,
+		CPU:       cpuInfo,
+		Memory:    memInfo,
+		Block:     blockInfo,
+		Topology:  topologyInfo,
+		Network:   netInfo,
+		GPU:       gpuInfo,
+		Chassis:   chassisInfo,
+		BIOS:      biosInfo,
+		Baseboard: baseboardInfo,
+		Product:   productInfo,
+	}, nil
+}
