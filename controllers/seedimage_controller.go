e802f7b3fef0aeb3ce94fbbdf834bf4a8e46ef5b
Francesco Giudici <francesco.giudici@suse.com>
2023-03-06 22:50:50 +0100

    SeedImage: add Pod and Service creation in the reconcile loop

--- controllers/seedimage_controller.go
+++ controllers/seedimage_controller.go
@@ -22,7 +22,12 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	errorutils "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -96,13 +101,216 @@ func (r *SeedImageReconciler) reconcile(ctx context.Context, seedImg *elementalv
 	logger.Info("Reconciling seedimage object")
 
 	if seedImg.GetDeletionTimestamp() != nil {
-		// TODO: r.reconcileDelete()
-		return ctrl.Result{}, nil
+		return r.reconcileDelete(ctx, seedImg)
 	}
 
 	controllerutil.AddFinalizer(seedImg, elementalv1.SeedImageFinalizer)
 
-	// TODO: add logic to manage the Pod
+	if err := r.createBuildImagePod(ctx, seedImg); err != nil {
+		meta.SetStatusCondition(&seedImg.Status.Conditions, metav1.Condition{
+			Type:    elementalv1.ReadyCondition,
+			Status:  metav1.ConditionFalse,
+			Reason:  elementalv1.PodCreationFailureReason,
+			Message: err.Error(),
+		})
+		return ctrl.Result{}, fmt.Errorf("failed to create pod: %w", err)
+	}
+
+	if err := r.createBuildImageService(ctx, seedImg); err != nil {
+		meta.SetStatusCondition(&seedImg.Status.Conditions, metav1.Condition{
+			Type:    elementalv1.ReadyCondition,
+			Status:  metav1.ConditionFalse,
+			Reason:  elementalv1.ServiceCreationFailureReason,
+			Message: err.Error(),
+		})
+		return ctrl.Result{}, fmt.Errorf("failed to create service: %w", err)
+	}
+
+	meta.SetStatusCondition(&seedImg.Status.Conditions, metav1.Condition{
+		Type:   elementalv1.ReadyCondition,
+		Reason: elementalv1.ResourcesSuccessfullyCreatedReason,
+		Status: metav1.ConditionTrue,
+	})
 
 	return ctrl.Result{}, nil
+}
+
+func (r *SeedImageReconciler) reconcileDelete(ctx context.Context, seedImg *elementalv1.SeedImage) (ctrl.Result, error) {
+	logger := ctrl.LoggerFrom(ctx)
+
+	logger.Info("Deleting pod and service resources")
+
+	if err := r.Client.Delete(ctx, &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: seedImg.Namespace,
+			Name:      seedImg.Name,
+		}}); err != nil && !apierrors.IsNotFound(err) {
+		return ctrl.Result{}, fmt.Errorf("failed to delete pod: %w", err)
+	}
+
+	if err := r.Client.Delete(ctx, &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: seedImg.Namespace,
+			Name:      seedImg.Name,
+		}}); err != nil && !apierrors.IsNotFound(err) {
+		return ctrl.Result{}, fmt.Errorf("failed to delete service: %w", err)
+	}
+
+	controllerutil.RemoveFinalizer(seedImg, elementalv1.SeedImageFinalizer)
+
+	return ctrl.Result{}, nil
+}
+
+func (r *SeedImageReconciler) createBuildImagePod(ctx context.Context, seedImg *elementalv1.SeedImage) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	logger.Info("Reconciling POD resources")
+
+	mRegistration := &elementalv1.MachineRegistration{}
+	if err := r.Get(ctx, types.NamespacedName{
+		Name:      seedImg.Spec.MachineRegistrationRef.Name,
+		Namespace: seedImg.Spec.MachineRegistrationRef.Namespace,
+	}, mRegistration); err != nil {
+		return err
+	}
+
+	logger.V(5).Info("Creating pod")
+
+	pod := fillBuildImagePod(seedImg.Name, seedImg.Namespace,
+		seedImg.Spec.BaseImage, mRegistration.Status.RegistrationURL)
+	pod.OwnerReferences = []metav1.OwnerReference{
+		{
+			APIVersion: elementalv1.GroupVersion.String(),
+			Kind:       "SeedImage",
+			Name:       seedImg.Name,
+			UID:        seedImg.UID,
+			Controller: pointer.Bool(true),
+		},
+	}
+
+	if err := r.Create(ctx, pod); err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	return nil
+}
+
+func fillBuildImagePod(name, namespace, seedImgURL, regURL string) *corev1.Pod {
+	const buildImage = "registry.opensuse.org/isv/rancher/elemental/stable/teal53/15.4/rancher/elemental-builder-image/5.3:latest"
+	const serveImage = "registry.opensuse.org/opensuse/nginx:latest"
+	const volLim = 4 * 1024 * 1024 * 1024 // 4 GiB
+	const volRes = 2 * 1024 * 1024 * 1024 // 2 GiB
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    map[string]string{"app.kubernetes.io/name": name},
+		},
+		Spec: corev1.PodSpec{
+			InitContainers: []corev1.Container{
+				{
+					Name:  "build",
+					Image: buildImage,
+					Resources: corev1.ResourceRequirements{
+						Limits: corev1.ResourceList{
+							corev1.ResourceEphemeralStorage: *resource.NewQuantity(volLim, resource.BinarySI),
+						},
+						Requests: corev1.ResourceList{
+							corev1.ResourceEphemeralStorage: *resource.NewQuantity(volRes, resource.BinarySI),
+						},
+					},
+					Command: []string{"/bin/bash", "-c"},
+					Args: []string{
+						fmt.Sprintf("%s; %s; %s",
+							fmt.Sprintf("curl -Lo base.img %s", seedImgURL),
+							fmt.Sprintf("curl -ko reg.yaml %s", regURL),
+							"xorriso -indev base.img -outdev /iso/elemental.iso -map reg.yaml /reg.yaml -boot_image any replay"),
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      "iso-storage",
+							MountPath: "/iso",
+						},
+					},
+				},
+			},
+			Containers: []corev1.Container{
+				{
+					Name:  "serve",
+					Image: serveImage,
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "http",
+							ContainerPort: 80,
+						},
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      "iso-storage",
+							MountPath: "/srv/www/htdocs",
+						},
+					},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyNever,
+			Volumes: []corev1.Volume{
+				{
+					Name: "iso-storage",
+					VolumeSource: corev1.VolumeSource{
+						EmptyDir: &corev1.EmptyDirVolumeSource{
+							SizeLimit: resource.NewQuantity(volLim, resource.BinarySI),
+						},
+					},
+				},
+			},
+		},
+	}
+	return pod
+}
+
+func (r *SeedImageReconciler) createBuildImageService(ctx context.Context, seedImg *elementalv1.SeedImage) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	logger.Info("Reconciling Service resources")
+
+	logger.V(5).Info("Creating service")
+
+	service := fillBuildImageService(seedImg.Name, seedImg.Namespace)
+	service.OwnerReferences = []metav1.OwnerReference{
+		{
+			APIVersion: elementalv1.GroupVersion.String(),
+			Kind:       "SeedImage",
+			Name:       seedImg.Name,
+			UID:        seedImg.UID,
+			Controller: pointer.Bool(true),
+		},
+	}
+
+	if err := r.Create(ctx, service); err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	return nil
+}
+
+func fillBuildImageService(name, namespace string) *corev1.Service {
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{"app.kubernetes.io/name": name},
+			Ports: []corev1.ServicePort{
+				{
+					Protocol: corev1.ProtocolTCP,
+					Port:     80,
+				},
+			},
+			Type: corev1.ServiceTypeNodePort,
+		},
+	}
+
+	return service
 }
