71ac99f7b621996c82f2a7d60227b6f2afbdbdc9
David Cassany <dcassany@suse.com>
2023-03-24 09:23:11 +0100

    Enble cache for MachineInventorySelector resources
    Signed-off-by: David Cassany <dcassany@suse.com>

--- cmd/operator/operator/root.go
+++ cmd/operator/operator/root.go
@@ -181,7 +181,6 @@ func operatorRun(config *rootConfig) {
 			&corev1.ConfigMap{},
 			&corev1.Secret{},
 			&elementalv1.ManagedOSVersion{},
-			&elementalv1.MachineInventorySelector{},
 		},
 		Port:                   config.webhookPort,
 		CertDir:                config.webhookCertDir,
