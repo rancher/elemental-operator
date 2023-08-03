b00495b49d4f3fc82ba614e5a1432ea8727b9b47
Andrea Mazzotti <andrea.mazzotti@suse.com>
2023-08-03 15:36:17 +0200

    Add verify generate and copyright to mock files

--- scripts/generate_mocks.sh
+++ scripts/generate_mocks.sh
@@ -5,7 +5,7 @@ go install go.uber.org/mock/mockgen@latest
 # Always create mock files into a "mocks" subfolder to be ignored in test coverage.
 # See codecov.yml for more info 
 
-mockgen -destination=pkg/register/mocks/client.go -package=mocks github.com/rancher/elemental-operator/pkg/register Client
-mockgen -destination=pkg/register/mocks/state.go -package=mocks github.com/rancher/elemental-operator/pkg/register StateHandler
-mockgen -destination=pkg/install/mocks/install.go -package=mocks github.com/rancher/elemental-operator/pkg/install Installer
-mockgen -destination=pkg/elementalcli/mocks/elementalcli.go -package=mocks github.com/rancher/elemental-operator/pkg/elementalcli Runner
+mockgen -copyright_file=scripts/boilerplate.go.txt -destination=pkg/register/mocks/client.go -package=mocks github.com/rancher/elemental-operator/pkg/register Client
+mockgen -copyright_file=scripts/boilerplate.go.txt -destination=pkg/register/mocks/state.go -package=mocks github.com/rancher/elemental-operator/pkg/register StateHandler
+mockgen -copyright_file=scripts/boilerplate.go.txt -destination=pkg/install/mocks/install.go -package=mocks github.com/rancher/elemental-operator/pkg/install Installer
+mockgen -copyright_file=scripts/boilerplate.go.txt -destination=pkg/elementalcli/mocks/elementalcli.go -package=mocks github.com/rancher/elemental-operator/pkg/elementalcli Runner
