00a02be000eba4ea98700d27be9e2876933c688f
Francesco Giudici <francesco.giudici@suse.com>
2023-09-05 16:32:53 +0200

    elemental-airgap: pick the operator chart as an argument
    pick the chart as a mandatory argument.
    If the argument is a path, download the chart. Otherwise consider it an
    already downloaded file.
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- scripts/elemental-airgap.sh
+++ scripts/elemental-airgap.sh
@@ -3,38 +3,40 @@
 set -e
 set -o pipefail
 
-: ${EOC_REGISTRY:="oci://registry.opensuse.org/isv/rancher/elemental/dev/charts/rancher"}
-: ${EOC_OPERATOR_CHART:=$EOC_REGISTRY/elemental-operator-chart}
-: ${EOC_CRDS_CHART:=$EOC_REGISTRY/elemental-operator-crds-chart}
+# global vars
+CHART_NAME_OPERATOR=""
+ELEMENTAL_OPERATOR_CRDS_CHART_NAME="\$ELEMENTAL_CRDS_CHART"
+
 : ${CONTAINER_IMAGES_NAME:="elemental-image"}
 : ${CONTAINER_IMAGES_FILE:=$CONTAINER_IMAGES_NAME".txt"}
 : ${CONTAINER_IMAGES_ARCHIVE:=$CONTAINER_IMAGES_NAME".tar.gz"}
 : ${DEBUG:="false"}
 : ${LOCAL_REGISTRY:=\$LOCAL_REGISTRY}
-
-# global vars
-CHART_NAME_OPERATOR=""
-CHART_NAME_CRDS=""
+: ${CHART_NAME_CRDS:=$ELEMENTAL_OPERATOR_CRDS_CHART_NAME}
+: ${CHART_VERSION:="latest"}
 
 print_help() {
     cat <<- EOF
-Usage: $0 [--chart-repo $EOC_REGISTRY]
-    [-c|--chart-repo path] source chart repo to pull elemental charts from (without the chart names).
+Usage: $0 [OPTION] ELEMENTAL_OPERATOR_CHART
     [-l|--image-list path] generated text file with the list of saved images (one image per line).
     [-i|--images path] tar.gz gernerated by docker save.
+    [-c|--crds-chart] Elemental CRDS chart (if URL, will be downloaded).
+    [-cv|--chart-version] Specify the chart version (only used if passing chart as urls).
+    [-r|--local-registry] registry where to load the images to (used in the next steps).
     [-d|--debug] enable debug output on screen.
     [-h|--help] Usage message.
 
     Parameters could also be set passing env vars:
-    EOC_REGISTRY (-c)               : $EOC_REGISTRY
-    EOC_OPERATOR_CHART              : $EOC_OPERATOR_CHART
-    EOC_CRDS_CHART                  : $EOC_CRDS_CHART
     CONTAINER_IMAGES_NAME           : $CONTAINER_IMAGES_NAME
     CONTAINER_IMAGES_FILE (-l)      : $CONTAINER_IMAGES_FILE
     CONTAINER_IMAGES_ARCHIVE (-i)   : $CONTAINER_IMAGES_ARCHIVE
     DEBUG (-d)                      : $DEBUG
-    LOCAL_REGISTRY                  : $LOCAL_REGISTRY
+    LOCAL_REGISTRY (-r)             : $LOCAL_REGISTRY
+    CHART_NAME_CRDS (-c)            : $CHART_NAME_CRDS
+    CHART_VERSION (-cv)             : $CHART_VERSION
 
+    example:
+    $0 oci://registry.opensuse.org/isv/rancher/elemental/staging/charts/rancher/elemental-operator-chart
 EOF
 }
 
@@ -56,10 +58,20 @@ parse_parameters() {
             shift # past argument
             shift # past value
             ;;
-            -s|--source-registry)
-            EOC_REGISTRY="$2"
-            shift # past argument
-            shift # past value
+            -c|--crds-chart)
+            CHART_NAME_CRDS="$2"
+            shift
+            shift
+            ;;
+            -cv|--chart-version)
+            CHART_VERSION="$2"
+            shift
+            shift
+            ;;
+            -r|--local-registry)
+            LOCAL_REGISTRY="$2"
+            shift
+            shift
             ;;
             -d|--debug)
             DEBUG="true"
@@ -70,8 +82,9 @@ parse_parameters() {
             shift
             ;;
             *)
-            print_help
-            exit 1
+            [ -n "$CHART_NAME_OPERATOR" ] && exit_error "unrecognized command: $1"
+            CHART_NAME_OPERATOR="$1"
+            shift
             ;;
         esac
     done
@@ -79,6 +92,11 @@ parse_parameters() {
     if [ "$help" = "true" ]; then
         print_help
         exit 0
+    fi
+    if [ -z "$CHART_NAME_OPERATOR" ]; then
+        print_help
+        echo ""
+        exit_error "ELEMENTAL_OPERATOR_CHART is a required argument"
     fi
 }
 
@@ -100,11 +118,15 @@ log_info() {
 }
 
 get_chart_val() {
-    var="$1"
-    val="$2"
+    local local_var="$1"
+    local local_val="$2"
+    local local_condition="[ \"\$$local_var\" = \"null\" ]"
 
-    eval $var=$(helm show values $CHART_NAME_OPERATOR | eval yq eval '.${val}' | sed s/\"//g 2>&1)
-    eval log_debug \"extracted $var\\t: \$$var\ \($val\)\"
+    eval $local_var=$(helm show values $CHART_NAME_OPERATOR | eval yq eval '.${local_val}' | sed s/\"//g 2>&1)
+    if eval $local_condition ; then
+        exit_error "cannot find \$local_val in $CHART_NAME_OPERATOR (is it an elemental-operator chart?)"
+    fi
+    eval log_debug \"extracted $local_var\\t: \$$local_var\ \($local_val\)\"
 }
 
 prereq_checks() {
@@ -118,21 +140,34 @@ prereq_checks() {
 }
 
 fetch_charts() {
-    local outstr chart
+    local outstr charts="CHART_NAME_OPERATOR"
 
-    log_debug "fetching chart '$EOC_OPERATOR_CHART'"
-    if ! outstr=$(helm pull ${EOC_OPERATOR_CHART} 2>&1); then
-        exit_error "downloading ${EOC_OPERATOR_CHART}:\n $outstr"
-    fi
-    CHART_NAME_OPERATOR=$(ls -t1 | head -n 1)
-    log_info "Downloaded Elemental Operator chart: $CHART_NAME_OPERATOR"
+    [ "$CHART_NAME_CRDS" != "$ELEMENTAL_OPERATOR_CRDS_CHART_NAME" ] && charts="${charts} CHART_NAME_CRDS"
 
-    log_debug "fetching chart '$EOC_CRDS_CHART'"
-    if ! outstr=$(helm pull ${EOC_CRDS_CHART} 2>&1); then
-        exit_error "downloading ${EOC_CRDS_CHART}:\n $outstr"
-    fi
-    CHART_NAME_CRDS=$(ls -t1 | head -n 1)
-    log_info "Downloaded Elemental Operator CRDS chart: $CHART_NAME_CRDS"
+    for c in $charts; do
+        local chart chart_ver
+
+        # helm pull only supports smever tags: for the "latest" tag just don't put the version.
+        [ "$CHART_VERSION" != "latest" ] && chart_ver="--version $CHART_VERSION"
+
+        # 'c' var holds the name (e.g., CHART_NAME_OPERATOR),
+        # 'chart' var holds the value (e.g., elemental-operator-chart-1.4.tgz)
+        eval chart=\$$c
+        case $chart in
+            "oci://"*|"https//"*)
+            log_debug "fetching chart '$chart' $chart_ver"
+            if ! outstr=$(helm pull ${chart} $chart_ver 2>&1); then
+                exit_error "downloading ${chart}:\n $outstr"
+            fi
+            eval $c=$(ls -t1 | head -n 1)
+            log_info "Downloaded Elemental Operator chart: $chart"
+            ;;
+            *)
+            [ ! -f "$chart" ] && exit_error "chart file $chart not found"
+            log_debug "using chart $chart"
+            ;;
+        esac
+    done
 }
 
 write_elemental_images_file() {
@@ -144,7 +179,7 @@ write_elemental_images_file() {
     get_chart_val seedimg_tag "seedImage.tag"
 
     log_info "Creating $CONTAINER_IMAGES_FILE"
-    cat <<- EOF >> $CONTAINER_IMAGES_FILE
+    cat <<- EOF > $CONTAINER_IMAGES_FILE
 ${oprtimg_repo}:${oprtimg_tag}
 ${seedimg_repo}:${seedimg_tag}
 EOF
