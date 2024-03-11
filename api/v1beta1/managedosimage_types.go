d7ad68a7d02466ebd2b35fcd4228b82918542ad1
Andrea Mazzotti <andrea.mazzotti@suse.com>
2024-03-11 14:23:17 +0100

    Make DrainSpec nullable
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>

--- api/v1beta1/managedosimage_types.go
+++ api/v1beta1/managedosimage_types.go
@@ -48,9 +48,9 @@ type ManagedOSImageSpec struct {
 	Prepare *upgradev1.ContainerSpec `json:"prepare,omitempty"`
 	// +optional
 	Cordon *bool `json:"cordon,omitempty"`
-	// +optional
+	// +nullable
 	// +kubebuilder:default:={"force":true,"skipWaitForDeleteTimeout":60}
-	Drain *upgradev1.DrainSpec `json:"drain,omitempty"`
+	Drain *upgradev1.DrainSpec `json:"drain"`
 	// +optional
 	UpgradeContainer *upgradev1.ContainerSpec `json:"upgradeContainer,omitempty"`
 	// +optional
