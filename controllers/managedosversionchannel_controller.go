a67e5d6e0515162b2ed5e5f30c3c03583e59ec84
Francesco Giudici <francesco.giudici@suse.com>
2024-06-20 14:41:43 +0200

    operator: always update managedosversionchannel last sync time
    ...also if we have an error on Pod creation.
    
    Fixes commit 5ccde141e687e20788acb62542660369b9e47861
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- controllers/managedosversionchannel_controller.go
+++ controllers/managedosversionchannel_controller.go
@@ -448,6 +448,12 @@ func (r *ManagedOSVersionChannelReconciler) createSyncerPod(ctx context.Context,
 		},
 	}
 
+	// If we don't update the LastSyncedTime on Pod creation, we will loop
+	// creating/deleting the Pod after the ManagedOSVersionChannel.spec.syncInterval
+	// see https://github.com/rancher/elemental-operator/issues/766
+	now := metav1.Now()
+	ch.Status.LastSyncedTime = &now
+
 	err := r.Create(ctx, pod)
 	if err != nil {
 		logger.Error(err, "Failed creating pod", "pod", ch.Name)
@@ -462,12 +468,6 @@ func (r *ManagedOSVersionChannelReconciler) createSyncerPod(ctx context.Context,
 		ch.Status.FailedSynchronizationAttempts++
 		return err
 	}
-
-	// If we don't update the LastSyncedTime on Pod creation, we will loop
-	// creating/deleting the Pod after the ManagedOSVersionChannel.spec.syncInterval
-	// see https://github.com/rancher/elemental-operator/issues/766
-	now := metav1.Now()
-	ch.Status.LastSyncedTime = &now
 
 	meta.SetStatusCondition(&ch.Status.Conditions, metav1.Condition{
 		Type:    elementalv1.ReadyCondition,
