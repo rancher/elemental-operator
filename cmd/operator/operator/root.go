2c44ae3cc37bf5f755ec897216cd5ca7a9c5cb8a
Andrea Mazzotti <andrea.mazzotti@suse.com>
2024-06-25 17:24:53 +0200

    Add managedosversion finalizer (#775)
    * Implement ManagedOSVersion controller and finalizer
    
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>
    
    * Remove unused ManagedOSVersion Status
    
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>
    ---------
    
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>
    Co-authored-by: Francesco Giudici <francesco.giudici@gmail.com>

--- cmd/operator/operator/root.go
+++ cmd/operator/operator/root.go
@@ -315,4 +315,10 @@ func setupReconcilers(mgr ctrl.Manager, config *rootConfig) {
 		setupLog.Error(err, "unable to create reconciler", "controller", "SeedImage")
 		os.Exit(1)
 	}
+	if err := (&controllers.ManagedOSVersionReconciler{
+		Client: mgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create reconciler", "controller", "ManagedOSVersion")
+		os.Exit(1)
+	}
 }
