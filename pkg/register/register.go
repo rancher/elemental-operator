cfd9780ecd4316170315dc8a70d225232c9fc5bb
Francesco Giudici <francesco.giudici@suse.com>
2024-09-11 15:58:21 +0200

    register: send both legacy and new Label Templates variables
    we don't want to break possible setups using the older Label Templates
    variables: send both.
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- pkg/register/register.go
+++ pkg/register/register.go
@@ -274,6 +274,10 @@ func sendSystemData(conn *websocket.Conn, protoVersion MessageType) error {
 	if protoVersion >= MsgSystemDataV2 {
 		log.Info("Sending System Data")
 		labels := hostinfo.ExtractLabels(data)
+		// Add legacy labels too (to be deprecated and removed sooner or later)
+		for k, v := range hostinfo.ExtractLabelsLegacy(data) {
+			labels[k] = v
+		}
 		if err != nil {
 			return fmt.Errorf("extracting labels from system data: %w", err)
 		}
