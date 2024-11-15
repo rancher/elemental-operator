37982fa762bd364a46dab7aa192191c8503e0f87
Francesco Giudici <francesco.giudici@suse.com>
2024-11-15 18:33:00 +0100

    util/net: rework to allow mocking system ip address retrieval
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- pkg/util/net.go
+++ pkg/util/net.go
@@ -17,8 +17,55 @@ limitations under the License.
 package util
 
 import (
+	"bytes"
 	"net"
+	"os/exec"
 )
+
+type NetController interface {
+	GetInterfaceAddresses(ifName string) [](net.IPNet)
+	GetNMDeviceState() (*bytes.Buffer, error)
+}
+
+func NewNetController() NetController {
+	return &unixNet{}
+}
+
+var _ NetController = (*unixNet)(nil)
+
+type unixNet struct{}
+
+func (un *unixNet) GetInterfaceAddresses(ifName string) (res []net.IPNet) {
+	var (
+		iface *net.Interface
+		addrs []net.Addr
+		err   error
+	)
+	res = []net.IPNet{}
+
+	if iface, err = net.InterfaceByName(ifName); err != nil {
+		return
+	}
+	if addrs, err = iface.Addrs(); err != nil {
+		return
+	}
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok {
+			res = append(res, *ipNet)
+		}
+	}
+	return
+}
+
+func (un *unixNet) GetNMDeviceState() (buf *bytes.Buffer, err error) {
+	buf = &bytes.Buffer{}
+	cmd := exec.Command("nmcli", "-g", "DEVICE,STATE", "device")
+	cmd.Stdout = buf
+
+	err = cmd.Run()
+
+	return
+}
 
 // GetIPByIfName returns a string containing the IP address of the network
 // interface name (ifName) passed as argument.
@@ -27,40 +74,29 @@ import (
 // This function cannot fail: if no address can be retrieved an empty string
 // is returned.
 func GetIPByIfName(ifName string) string {
+	return getIPByIfName(ifName, NewNetController())
+}
+
+func getIPByIfName(ifName string, nc NetController) string {
 	var (
-		iface   *net.Interface
-		addrs   []net.Addr
-		ip, ip6 net.IP
-		err     error
+		ips  []net.IPNet
+		ipv6 net.IP
 	)
-
-	if iface, err = net.InterfaceByName(ifName); err != nil {
+	if ips = nc.GetInterfaceAddresses(ifName); len(ips) == 0 {
 		return ""
 	}
-	if addrs, err = iface.Addrs(); err != nil {
-		return ""
-	}
-	for _, addr := range addrs {
-		var (
-			ipNet *net.IPNet
-			ok    bool
-		)
-
-		if ipNet, ok = addr.(*net.IPNet); !ok {
-			continue
-		}
-
-		if ip = ipNet.IP.To4(); ip != nil {
-			return ip.String()
+	for _, ip := range ips {
+		if ipv4 := ip.IP.To4(); ipv4 != nil {
+			return ipv4.String()
 		}
 
 		// Let's keep an IPv6 address as fallback if the interface has no IPv4 addresses
-		if ip6 == nil {
-			ip6 = ipNet.IP.To16()
+		if ipv6 == nil {
+			ipv6 = ip.IP.To16()
 		}
 	}
-	if ip6 != nil {
-		return ip6.String()
+	if ipv6 != nil {
+		return ipv6.String()
 	}
 	return ""
 }
