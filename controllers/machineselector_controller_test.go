e6e58f97f2d386f3c84e1245552b43438b6ab178
David Cassany <dcassany@suse.com>
2026-02-20 13:41:34 +0100

    Bump to cluster API libraries to v1.12.x
    This library bump includes moving from v1beta1 API to
    v1beta2 API.
    
    Signed-off-by: David Cassany <dcassany@suse.com>
    (cherry picked from commit 15bb505fe81693d80b4762f1242c80db0aad949f)

--- controllers/machineselector_controller_test.go
+++ controllers/machineselector_controller_test.go
@@ -26,7 +26,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
-	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 
 	elementalv1 "github.com/rancher/elemental-operator/api/v1beta1"
 	"github.com/rancher/elemental-operator/pkg/test"
