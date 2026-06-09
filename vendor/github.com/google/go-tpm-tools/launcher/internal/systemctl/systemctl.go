9b8c997915b7e7e9a7d43fef6eb05ac723fe417f
Bharti Gautam <bharti.gautam@suse.com>
2026-06-09 22:50:39 +0530

    ran make vendor

---
+++ vendor/github.com/google/go-tpm-tools/launcher/internal/systemctl/systemctl.go
@@ -0,0 +1,77 @@
+// Package systemctl implements a subset of systemctl operations.
+package systemctl
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/coreos/go-systemd/v22/dbus"
+)
+
+// Systemd is an interface to connect to host systemd with selected functions.
+type Systemd interface {
+	Start(string) error
+	Stop(string) error
+	IsActive(context.Context, string) (string, error)
+	Close()
+}
+
+// Systemctl is a wrap around of dbus.Conn and implements the Systemd interface.
+type Systemctl struct {
+	dbus *dbus.Conn
+}
+
+var _ Systemd = (*Systemctl)(nil)
+
+// New connects to systemd over dbus.
+func New() (*Systemctl, error) {
+	conn, err := dbus.NewWithContext(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return &Systemctl{dbus: conn}, nil
+}
+
+// Start is the equivalent of `systemctl start $unit`.
+func (s *Systemctl) Start(unit string) error {
+	return runSystemdCmd(s.dbus.StartUnitContext, "start", unit)
+}
+
+// Stop is the equivalent of `systemctl stop $unit`.
+func (s *Systemctl) Stop(unit string) error {
+	return runSystemdCmd(s.dbus.StopUnitContext, "stop", unit)
+}
+
+// IsActive is the equivalent of `systemctl is-active $unit`.
+// The status can be "active", "activating", "deactivating", "inactive" or "failed".
+func (s *Systemctl) IsActive(ctx context.Context, unit string) (string, error) {
+	status, err := s.dbus.ListUnitsByNamesContext(ctx, []string{unit})
+	if err != nil {
+		return "", err
+	}
+	if len(status) != 1 {
+		return "", fmt.Errorf("want 1 unit from ListUnitsByNames, got %d", len(status))
+	}
+	return status[0].ActiveState, nil
+}
+
+// Close disconnects from dbus.
+func (s *Systemctl) Close() { s.dbus.Close() }
+
+func runSystemdCmd(cmdFunc func(context.Context, string, string, chan<- string) (int, error), cmd string, unit string) error {
+	progress := make(chan string, 1)
+
+	// Run systemd command in "replace" mode to start the unit and its dependencies,
+	// possibly replacing already queued jobs that conflict with this.
+	if _, err := cmdFunc(context.Background(), unit, "replace", progress); err != nil {
+		return fmt.Errorf("failed to run systemctl [%s] for unit [%s]: %v", cmd, unit, err)
+	}
+
+	if result := <-progress; result != "done" {
+		return fmt.Errorf("systemctl [%s] result was [%s], want done", cmd, result)
+	}
+
+	log.Printf("Finished up systemctl [%s] for unit [%s]", cmd, unit)
+	return nil
+}
---
+++ vendor/github.com/google/go-tpm-tools/launcher/teeserver/tee_server.go
@@ -0,0 +1,238 @@
+// Package teeserver implements a server to be run in the launcher.
+// Used for communicate between the host/launcher and the container.
+package teeserver
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/google/go-tpm-tools/launcher/agent"
+	"github.com/google/go-tpm-tools/launcher/internal/logging"
+	"github.com/google/go-tpm-tools/launcher/spec"
+	"github.com/google/go-tpm-tools/verifier"
+	"github.com/google/go-tpm-tools/verifier/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	gcaEndpoint = "/v1/token"
+	itaEndpoint = "/v1/intel/token"
+)
+
+var clientErrorCodes = map[codes.Code]struct{}{
+	codes.InvalidArgument:    {},
+	codes.FailedPrecondition: {},
+	codes.PermissionDenied:   {},
+	codes.Unauthenticated:    {},
+	codes.NotFound:           {},
+	codes.Aborted:            {},
+	codes.OutOfRange:         {},
+	codes.Canceled:           {},
+}
+
+// AttestClients contains clients for supported verifier services that can be used to
+// get attestation tokens.
+type AttestClients struct {
+	GCA verifier.Client
+	ITA verifier.Client
+}
+
+type attestHandler struct {
+	ctx         context.Context
+	attestAgent agent.AttestationAgent
+	// defaultTokenFile string
+	logger     logging.Logger
+	launchSpec spec.LaunchSpec
+	clients    AttestClients
+}
+
+// TeeServer is a server that can be called from a container through a unix
+// socket file.
+type TeeServer struct {
+	server      *http.Server
+	netListener net.Listener
+}
+
+// New takes in a socket and start to listen to it, and create a server
+func New(ctx context.Context, unixSock string, a agent.AttestationAgent, logger logging.Logger, launchSpec spec.LaunchSpec, clients AttestClients) (*TeeServer, error) {
+	var err error
+	nl, err := net.Listen("unix", unixSock)
+	if err != nil {
+		return nil, fmt.Errorf("cannot listen to the socket [%s]: %v", unixSock, err)
+	}
+
+	teeServer := TeeServer{
+		netListener: nl,
+		server: &http.Server{
+			Handler: (&attestHandler{
+				ctx:         ctx,
+				attestAgent: a,
+				logger:      logger,
+				launchSpec:  launchSpec,
+				clients:     clients,
+			}).Handler(),
+		},
+	}
+	return &teeServer, nil
+}
+
+// Handler creates a multiplexer for the server.
+func (a *attestHandler) Handler() http.Handler {
+	mux := http.NewServeMux()
+	// to test default token: curl --unix-socket <socket> http://localhost/v1/token
+	// to test custom token:
+	// curl -d '{"audience":"<aud>", "nonces":["<nonce1>"]}' -H "Content-Type: application/json" -X POST
+	//   --unix-socket /tmp/container_launcher/teeserver.sock http://localhost/v1/token
+
+	mux.HandleFunc(gcaEndpoint, a.getToken)
+	mux.HandleFunc(itaEndpoint, a.getITAToken)
+	return mux
+}
+
+func (a *attestHandler) logAndWriteError(errStr string, status int, w http.ResponseWriter) {
+	a.logger.Error(errStr)
+	w.WriteHeader(status)
+	w.Write([]byte(errStr))
+}
+
+// getDefaultToken handles the request to get the default OIDC token.
+// For now this function will just read the content of the file and return.
+// Later, this function can use attestation agent to get a token directly.
+func (a *attestHandler) getToken(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	a.logger.Info(fmt.Sprintf("%s called", gcaEndpoint))
+
+	// If the handler does not have an GCA client, return error.
+	if a.clients.GCA == nil {
+		errStr := "no GCA verifier client present, please try rebooting your VM"
+		a.logAndWriteError(errStr, http.StatusInternalServerError, w)
+		return
+	}
+
+	a.attest(w, r, a.clients.GCA)
+}
+
+// getITAToken retrieves a attestation token signed by ITA.
+func (a *attestHandler) getITAToken(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	a.logger.Info(fmt.Sprintf("%s called", itaEndpoint))
+
+	// If the handler does not have an ITA client, return error.
+	if a.clients.ITA == nil {
+		errStr := "no ITA verifier client present - ensure ITA Region and Key are defined in metadata"
+		a.logAndWriteError(errStr, http.StatusInternalServerError, w)
+		return
+	}
+
+	a.attest(w, r, a.clients.ITA)
+}
+
+func (a *attestHandler) attest(w http.ResponseWriter, r *http.Request, client verifier.Client) {
+	switch r.Method {
+	case http.MethodGet:
+		if err := a.attestAgent.Refresh(a.ctx); err != nil {
+			a.logAndWriteHTTPError(w, http.StatusInternalServerError, fmt.Errorf("failed to refresh attestation agent: %w", err))
+			return
+		}
+
+		token, err := a.attestAgent.AttestWithClient(a.ctx, agent.AttestAgentOpts{}, client)
+		if err != nil {
+			a.handleAttestError(w, err, "failed to retrieve attestation service token")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(token)
+		return
+
+	case http.MethodPost:
+		var tokenOptions models.TokenOptions
+		decoder := json.NewDecoder(r.Body)
+		decoder.DisallowUnknownFields()
+
+		err := decoder.Decode(&tokenOptions)
+		if err != nil {
+			err = fmt.Errorf("failed to parse POST body as TokenOptions: %v", err)
+			a.logAndWriteHTTPError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		if tokenOptions.Audience == "" {
+			err := fmt.Errorf("use GET request for the default identity token")
+			a.logAndWriteHTTPError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		if tokenOptions.TokenType == "" {
+			err := fmt.Errorf("token_type is a required parameter")
+			a.logAndWriteHTTPError(w, http.StatusBadRequest, err)
+			return
+		}
+
+		// Do not check that TokenTypeOptions matches TokenType in the launcher.
+		opts := agent.AttestAgentOpts{
+			TokenOptions: &tokenOptions,
+		}
+		tok, err := a.attestAgent.AttestWithClient(a.ctx, opts, client)
+		if err != nil {
+			a.handleAttestError(w, err, "failed to retrieve custom attestation service token")
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(tok)
+		return
+	default:
+		// TODO: add an url pointing to the REST API document
+		err := fmt.Errorf("TEE server received an invalid HTTP method: %s", r.Method)
+		a.logAndWriteHTTPError(w, http.StatusBadRequest, err)
+	}
+}
+
+func (a *attestHandler) logAndWriteHTTPError(w http.ResponseWriter, statusCode int, err error) {
+	a.logger.Error(err.Error())
+	w.WriteHeader(statusCode)
+	w.Write([]byte(err.Error()))
+}
+
+// Serve starts the server, will block until the server shutdown.
+func (s *TeeServer) Serve() error {
+	return s.server.Serve(s.netListener)
+}
+
+// Shutdown will terminate the server and the underlying listener.
+func (s *TeeServer) Shutdown(ctx context.Context) error {
+	err := s.server.Shutdown(ctx)
+	err2 := s.netListener.Close()
+
+	if err != nil {
+		return err
+	}
+	if err2 != nil {
+		return err2
+	}
+	return nil
+}
+
+func (a *attestHandler) handleAttestError(w http.ResponseWriter, err error, message string) {
+	st, ok := status.FromError(err)
+	if ok {
+		if _, exists := clientErrorCodes[st.Code()]; exists {
+			// User errors, like invalid arguments. Map user errors to 400 Bad Request.
+			a.logAndWriteHTTPError(w, http.StatusBadRequest, fmt.Errorf("%s: %w", message, err))
+			return
+		}
+		// Server-side or transient errors. Map user errors 500 Internal Server Error.
+		a.logAndWriteHTTPError(w, http.StatusInternalServerError, fmt.Errorf("%s: %w", message, err))
+		return
+	}
+	// If it's not a gRPC error, it's likely an internal error within the launcher.
+	// Map user errors 500 Internal Server Error
+	a.logAndWriteHTTPError(w, http.StatusInternalServerError, fmt.Errorf("%s: %w", message, err))
+}
